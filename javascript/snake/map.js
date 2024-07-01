05a26931b05b81541eef98bf760960e3031619cb
王政 <everywangbug@gmail.com>
2024-07-02 00:54:04 +0800

    贪吃蛇样式搭建

---
+++ javascript/snake/map.js
@@ -0,0 +1,22 @@
+class Map {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+  }
+
+  generateMap() {
+    const tableEl = document.querySelector('.map')
+    for (let row = 0; row < this.height; row++) {
+      const trEl = document.createElement('tr')
+      for (let column = 0; column < this.width; column++) {
+        const tdEl = document.createElement('td');
+        tdEl.classList.add('cell')
+        trEl.appendChild(tdEl)
+      }
+      trEl.classList.add('row')
+      tableEl.appendChild(trEl)
+    }
+  }
+}
+
+const map = new Map(70, 35)
