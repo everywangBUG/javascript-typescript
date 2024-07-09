76835618e4d5d0f1ade4db1b49aa3a9dfc32ebc9
王政 <everywangbug@gmail.com>
2024-07-09 23:59:13 +0800

    蛇头蛇身gameOver方法

--- javascript/snake/snake.js
+++ javascript/snake/snake.js
@@ -5,20 +5,72 @@ class Snake {
       this.mapEl = document.querySelector(selector)
       this.direction = 'right'
       this.snake = []
-      this.createSnakeHead()
+      this.createSnake()
+      this.move()
+      console.log(this.map, 'this.map')
     }
 
     createSnakeHead() {
-      const x = Math.floor(Math.random() * this.map.width)
-      const y = Math.floor(Math.random() * this.map.height)
-      const { foodX, foodY } = this.food.foodPos
-      if (foodX !== x && foodY !== y) {
-        const snakeHead = document.createElement('div')
-        snakeHead.classList.add('snake_head')
-        snakeHead.style.left = `${x * 20}px`
-        snakeHead.style.top = `${y * 20}px`
-        this.mapEl.appendChild(snakeHead)
-        this.snake.unshift(snakeHead)
+      const head = this.snake[0]
+      const pos = { x: 0, y: 0 }
+
+      if (head) {
+        switch(this.direction) {
+          case 'right':
+            pos.x = head.offsetLeft + 20
+            pos.y = head.offsetTop
+            break
+          case 'left':
+            pos.x = head.offsetLeft - 20
+            pos.y = head.offsetTop
+            break
+          case 'up':
+            pos.x = head.offsetLeft
+            pos.y = head.offsetTop - 20
+            break
+          case 'down':
+            pos.x = head.offsetLeft
+            pos.y = head.offsetTop + 20
+            break
+        }
+        console.log(head, 'head')
+        head.className = 'snake_body'
       }
+      
+      const snakeHead = document.createElement('div')
+      snakeHead.classList.add('snake_head')
+      snakeHead.style.left = `${pos.x}px`
+      snakeHead.style.top = `${pos.y}px`
+      this.mapEl.appendChild(snakeHead)
+      this.snake.unshift(snakeHead)
+    }
+
+    createSnake() {
+      for(let i = 0; i <= 4; i++) {
+          this.createSnakeHead()
+      }
+    }
+
+    move() {
+      const body = this.snake.pop()
+      body.remove()
+      this.createSnakeHead()
+    }
+
+    isEatFood(foodX, foodY) {
+      const head = this.snake[0]
+      if (head.offsetLeft === foodX && head.offsetTop === foodY) {
+        return true
+      }
+      return false
+    }
+
+    isDie() {
+      // TODO: 判断蛇是否撞墙
+      const head = this.snake[0]
+      if (head.offsetLeft < 0 || head.offsetTop < 0 || head.offsetLeft > this.map.width * 20 || head.offsetTop > this.map.height * 20) {
+        return true
+      }
+      return false
     }
 }
